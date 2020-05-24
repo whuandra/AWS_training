c83dfad71e8390e337f912a5422f06c29fe67aee
whuandra <50607920+whuandra@users.noreply.github.com>
2020-05-24 12:16:43 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+}
+  
